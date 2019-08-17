475bf12db9bb3387fad38f95337893fdf39af534
tiagokxuera <tiagokxuera@hotmail.com>
2019-08-17 16:54:59 -0300

    CRUD complete

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -2,45 +2,74 @@ const express = require('express');
 const router = express.Router();
 
 const checkAuth = require('../middleware/check-auth');
+const notFound = require('../middleware/not-found');
 
+let db = {};
+let sequence = 0;
+
+//POST
 router.post('/', checkAuth,(request, response) => {
     //Criamos uma new Task
     const newTask = {
-        id: Date.now(),
+        id: ++sequence,
         done: request.body.done || false,
         description: request.body.description
-    }
-    response.status(201).json({
-        message: 'The task has been created',
-        newTask
-    });
-});
-
-router.get('/', (request, response) => {
-    response.status(200).json({
-      message: 'Tasks has been fetched'
-    });
-});
-
-router.get('/:taskId', (request, response) => {
-    const id = request.params.taskId;
-    response.status(200).json({
-      message: `Task with ID = ${id} was fetched`
-    });
-});      
-
-router.patch('/:taskId', checkAuth, (request, response) => {
-    const id = request.params.taskId;
-    response.status(200).json({
-      message: `Task with ID = ${id} was patched`
-    });
+    };
+    //acrescenta no banco em memória
+    db[newTask.id] = newTask;
+    //Devolve o newTask criado
+    response.status(201).json(newTask);
 }); 
 
-router.delete('/:taskId', checkAuth,(request, response) => {
-    const id = request.params.taskId;
-    response.status(200).json({
-      message: `Task with ID = ${id} has been deleted`
-    });
-});  
+//get all
+router.get('/', (request, response) => {
+    //response.status(200).json({
+    //  message: 'Tasks has been fetched'
+    //});
+    
+    //convert para array
+    const toArray = key => db[key];
+    const tasks = Object.keys(db).map(toArray);
+    
+    //Se tem tasks, retorna. Se não, 204
+    tasks.length
+        ? response.json(tasks)
+        : response.status(204).end();
+}); 
+
+//get
+router.get('/:taskId', (request, response) => {
+    const task = db[request.params.taskId];
+    task
+      ? response.json(task)
+      : notFound(request, response)
+});
+
+//Edit
+//Altero somente o que eu quero
+router.patch('/:taskId', checkAuth, (request, response) => {
+    const task = db[request.params.taskId];
+    if (task) {
+      task.done = request.body.done || task.done;
+      task.description = request.body.description || task.description;
+      response.json(task);
+    } else {
+      notFound(request, response);
+    }
+});
+   
+
+router.delete('/:taskId', checkAuth, (request, response) => {
+    const task = db[request.params.taskId];
+    if(task) {
+      delete db[task.id];
+      response.status(200).json({
+        message: 'Deleted'
+      });
+    } else {
+      notFound(request, response);
+    }
+});
+   
 
 module.exports = router;
