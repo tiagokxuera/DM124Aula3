9ff69f34b546b186502cae94d0336a2aa9b04820
tiagokxuera <tiagokxuera@hotmail.com>
2019-08-17 13:28:01 -0300

    morgan logs add

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
+app.use(morgan('dev'));
 
 app.use((request, response, next) => {
- console.log(request.url);
  response.status(200).json({
    message: 'It works baby!'
  });
