003c16b5a7a061b92146861f89d5ecd6bee45fff
tiagokxuera <tiagokxuera@hotmail.com>
2019-08-17 14:25:53 -0300

    crud routes

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (request, response) => {
-  response.status(200).json({
-    message: 'Handling POST requests to /tasks'
+  response.status(201).json({
+    message: 'The task has been created'
   });
 });
 
 router.get('/', (request, response) => {
     response.status(200).json({
-      message: 'Handling GET requests to /tasks'
+      message: 'Tasks has been fetched'
     });
 });
 
@@ -19,5 +19,19 @@ router.get('/:taskId', (request, response) => {
       message: `Task with ID = ${id} was fetched`
     });
 });      
+
+router.patch('/:taskId', (request, response) => {
+    const id = request.params.taskId;
+    response.status(200).json({
+      message: `Task with ID = ${id} was patched`
+    });
+}); 
+
+router.delete('/:taskId', (request, response) => {
+    const id = request.params.taskId;
+    response.status(200).json({
+      message: `Task with ID = ${id} has been deleted`
+    });
+});  
 
 module.exports = router;
